7e791b22756b3ea7724311401d175f6bd94a7ec8
aokad <aokada@ims.u-tokyo.ac.jp>
2017-05-19 14:33:30 +0900

    update

---
+++ graphs/style/default.js
@@ -0,0 +1,57 @@
+(function(){
+style = {
+    id_exome: 'exome',
+    id_wgs__: 'wgs',
+    id_targt: 'target',
+    id_rna_p: 'rna',
+    id_rna_s: 'rna-single',
+    id_mirna: 'mirna',
+    
+    label_exome: 'EXOME',
+    label_wgs__: 'WGS',
+    label_targt: 'TARGET',
+    label_rna_p: 'RNA',
+    label_rna_s: 'RNA-single',
+    label_mirna: 'miRNA',
+
+// Highcharts default color:
+// colors: ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1']
+
+    color_exome: '#7cb5ec', // blue
+    color_wgs__: '#90ed7d', // light-green
+    color_targt: '#8085e9', // purple
+    color_rna_p: '#f7a35c', // orange
+    color_rna_s: '#f15c80', // pink
+    color_mirna: '#e4d354', // yellow
+    color_dmmy1: '#2b908f', // dark-green
+    color_dmmy2: '#f45b5b', // pink
+    color_dmmy3: '#91e8e1', // light-blue
+
+// # Legacy
+// In Highcharts 3.x, the default colors were:
+// colors: ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce', '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a']
+//    color_exome: '#2f7ed8', // blue
+//    color_wgs__: '#0d233a', // black
+//    color_targt: '#8bbc21', // yellow-green
+//    color_rna_p: '#910000', // dark-red
+//    color_rna_s: '#1aadce', // light-blue
+//    color_mirna: '#492970', // dark-purple
+//    color_dmmy1: '#f28f43', // orange
+//    color_dmmy2: '#77a1e5', // blue
+//    color_dmmy3: '#c42525', // dark-cherry
+//    color_dmmy4: '#a6c96a', // yellow-greeen
+// 
+// In Highcharts 2.x, the default colors were:
+// colors: ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92']
+//    color_exome: '#4572A7', // dark-blue
+//    color_wgs__: '#AA4643', // dark-red
+//    color_targt: '#89A54E', // yellow-green
+//    color_rna_p: '#80699B', // purple
+//    color_rna_s: '#3D96AE', // light-blue
+//    color_mirna: '#DB843D', // orange
+//    color_dmmy1: '#92A8CD', // light-purple
+//    color_dmmy2: '#A47D7C', // light-brown
+//    color_dmmy3: '#B5CA92', // light-yellow-greeen
+
+};
+})();
